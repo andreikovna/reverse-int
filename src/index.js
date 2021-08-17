5b99064aa955ac685c6a74d7b146842410a8e8b6
andreikovna <d.andreikovna@gmail.com>
2021-08-18 00:13:34 +0300

    fix: add negative

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 module.exports = function reverse (n) {
   let reversed = 0;
+  if (n < 0) {
+    n = n * (-1);
+  }
+  
   while (n){
       reversed = reversed * 10 + n % 10;
       n = Math.floor(n/10);
